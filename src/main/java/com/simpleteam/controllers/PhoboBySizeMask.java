99c8f3b0674b94a12c13be77430ebef136fae210
Fiz <fanatic83@gmail.com>
2016-05-25 22:23:38 +0300

    controller by size

--- src/main/java/com/simpleteam/controllers/PhoboBySizeMask.java
+++ src/main/java/com/simpleteam/controllers/PhoboBySizeMask.java
@@ -1,6 +1,8 @@
 package com.simpleteam.controllers;
 
+import com.simpleteam.core.PhotoGallery;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,12 @@ public class PhoboBySizeMask {
     private final Logger log = Logger.getLogger(PhoboBySizeMask.class);
 
     /**
+     * Use PhotoGallery class.
+     */
+    @Autowired
+    private PhotoGallery gallery;
+
+    /**
      * Catch size mask of photo.
      * @param model Map for add attributes
      * @param sizeMask mask for photo size
@@ -26,7 +34,19 @@ public class PhoboBySizeMask {
     public final String bySize(final Model model, @PathVariable("sizeMask") final String sizeMask) {
         log.info("GET");
 
-        model.addAttribute("sizeMask", sizeMask);
+        int sizeX = 200;
+        int sizeY = 200;
+        try {
+            String[] sizeXY = sizeMask.split("x");
+            sizeX = Integer.parseInt(sizeXY[0]);
+            sizeY = Integer.parseInt(sizeXY[1]);
+        } catch (Exception e) {
+            log.error("Wrong size: " + sizeMask + "  ", e);
+        }
+
+        model.addAttribute("sizeX", sizeX)
+             .addAttribute("sizeY", sizeY)
+             .addAttribute("photos", new int[gallery.getPhotos().size()]);
         return "photoBySizeMask";
     }
 
